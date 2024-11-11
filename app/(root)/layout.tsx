1fb2e46d459b7b4ecd4859a9a1cf0fb761f28714
PRICIDON <dddbjjk6@gmail.com>
2024-11-11 21:50:40 +0300

    Вроде закончена загрузка файлов

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,24 +3,31 @@ import Sidebar from "@/components/Sidebar";
 import MobileNavigation from "@/components/MobileNavigation";
 import Header from "@/components/Header";
 import { getCurrentUser } from "@/lib/actions/user.actions";
-import { redirect } from "next/navigation";
 import { Toaster } from "@/components/ui/toaster";
 import { cookies } from "next/headers";
-
+import { redirect } from "next/navigation";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  const currentUser = await getCurrentUser();
-  const curUser = (await cookies()).get("appwrite-session");
-  if (!currentUser || !curUser) return redirect("/sign-in");
-  return (
-    <main className="flex h-screen">
-      <Sidebar {...currentUser} />
-      <section className="flex h-full flex-1 flex-col">
-        <MobileNavigation {...currentUser} />
-        <Header {...currentUser} />
-        <div className="main-content">{children}</div>
-      </section>{" "}
-      <Toaster />
-    </main>
-  );
+  try {
+    const currentUser = await getCurrentUser();
+    const sessionCookie = (await cookies()).get("appwrite-session");
+    if (!currentUser || !sessionCookie) {
+      return redirect("/sign-in");
+    }
+
+    return (
+      <main className="flex h-screen">
+        <Sidebar {...currentUser} />
+        <section className="flex h-full flex-1 flex-col">
+          <MobileNavigation {...currentUser} />
+          <Header {...currentUser} />
+          <div className="main-content">{children}</div>
+        </section>{" "}
+        <Toaster />
+      </main>
+    );
+  } catch (error) {
+    console.error("Error during layout rendering:", error);
+    return redirect("/sign-in");
+  }
 };
 export default Layout;
