77a2bc2b71d191bac5ee4939b8d1d602722f4ed4
PRICIDON <dddbjjk6@gmail.com>
2024-11-12 23:26:45 +0300

    Rename Files

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -7,26 +7,19 @@ import { Toaster } from "@/components/ui/toaster";
 import { redirect } from "next/navigation";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 const Layout = async ({ children }: { children: React.ReactNode }) => {
-  try {
-    const currentUser = await getCurrentUser();
-    if (!currentUser) {
-      console.log("Error during layout rendering");
-    }
-    return (
-      <main className="flex h-screen">
-        <Sidebar {...currentUser} />
-        <section className="flex h-full flex-1 flex-col">
-          <MobileNavigation {...currentUser} />
-          <Header {...currentUser} />
-          <div className="main-content">{children}</div>
-          <SpeedInsights />
-        </section>
-        <Toaster />
-      </main>
-    );
-  } catch (error) {
-    console.log("Error during layout rendering:", error);
-    redirect("/sign-in");
-  }
+  const currentUser = await getCurrentUser();
+  if (!currentUser) return redirect("/sign-in");
+  return (
+    <main className="flex h-screen">
+      <Sidebar {...currentUser} />
+      <section className="flex h-full flex-1 flex-col">
+        <MobileNavigation {...currentUser} />
+        <Header {...currentUser} />
+        <div className="main-content">{children}</div>
+        <SpeedInsights />
+      </section>
+      <Toaster />
+    </main>
+  );
 };
 export default Layout;
